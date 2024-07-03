c34daf2b73a69c739f25b5edb2bdaf4bcd723183
yura_filchakov <filchakov1993@gmail.com>
2024-07-03 23:10:48 +0700

    main

---
+++ main.py
@@ -0,0 +1,39 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+    
+    def get_context_data(self):
+        """
+        Функция для подключения и четения к HTML
+        """
+        with open('web.html', 'r', encoding='utf-8') as file:
+            contex = file.read()
+            return contex
+    
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(self.get_context_data(), "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+    
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    
+    webServer.server_close()
+    print("Server stopped.")
